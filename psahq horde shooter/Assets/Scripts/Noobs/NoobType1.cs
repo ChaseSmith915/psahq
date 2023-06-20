e9a8eca224cb688f9a10b45f06a016a9bf0d296e
ChaseSmith915 <82478282+ChaseSmith915@users.noreply.github.com>
2023-06-20 00:30:38 -0400

    fixed a bug and gave noob1 his animations

--- psahq horde shooter/Assets/Scripts/Noobs/NoobType1.cs
+++ psahq horde shooter/Assets/Scripts/Noobs/NoobType1.cs
@@ -1,4 +1,5 @@
 using Photon.Pun;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,6 +9,20 @@ public class NoobType1 : Noob, IPunObservable
     //There is no Start method because the Noob abstract class has one for it.
     //But you may override it if you want.
 
+    [SerializeField] private Animator anim;
+
+    public override void Start()
+    {
+        base.Start();
+        try
+        {
+            anim.SetInteger("skin", UnityEngine.Random.Range(1, 5));
+        }
+        catch(Exception ex)
+        {
+
+        }
+    }
     // Update is called once per frame
     void Update()
     {
