d27f9f85e9edc0b0cfc72da175f25fd9b7359312
ChaseSmith915 <82478282+ChaseSmith915@users.noreply.github.com>
2023-06-24 00:10:07 -0400

    started skeng implementation

--- psahq horde shooter/Assets/Scripts/Networking/OnSceneLoad.cs
+++ psahq horde shooter/Assets/Scripts/Networking/OnSceneLoad.cs
@@ -6,9 +6,22 @@ using Photon.Pun;
 public class OnGameStart : MonoBehaviour
 {
     [SerializeField] private GameObject playerPrefab; //I wonder what horse ross thinks of this
+    [SerializeField] private GameObject skengPrefab; //I wonder what Inversonia thinks of this
     void Start()
     {
-        PhotonNetwork.Instantiate(playerPrefab.name, new Vector2(UnityEngine.Random.Range(-5f, 5f), transform.position.y), Quaternion.identity);
+        switch(GlobalVariables.Instance.characterChoice)
+        {
+            case "Ross":
+                {
+                    PhotonNetwork.Instantiate(playerPrefab.name, new Vector2(UnityEngine.Random.Range(-5f, 5f), transform.position.y), Quaternion.identity);
+                }
+                break;
+            case "Skeng":
+                {
+                    PhotonNetwork.Instantiate(skengPrefab.name, new Vector2(UnityEngine.Random.Range(-5f, 5f), transform.position.y), Quaternion.identity);
+                }
+                break;
+        }
     }
 
     // Update is called once per frame
